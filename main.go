88c92cf8ff13859c907515eb4fb189f7624960de
KeisukeToyota <hm.pudding0715@gmail.com>
2019-12-03 18:53:47 +0900

    v0.3.6

--- main.go
+++ main.go
@@ -46,7 +46,7 @@ func main() {
 					 |___/`
 
 	app.Usage = ""
-	app.Version = "0.3.5"
+	app.Version = "0.3.6"
 	app.Commands = []cli.Command{
 		commands.TweetCommand(api, v),
 		commands.TimelineCommand(api, v),
