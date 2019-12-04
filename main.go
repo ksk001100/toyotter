58e196aeb932a2efedda13c6e69a6ba8cd9f9ae3
ksk001100 <hm.pudding0715@gmail.com>
2019-12-04 13:31:31 +0900

    v0.4.0

--- main.go
+++ main.go
@@ -46,7 +46,7 @@ func main() {
 					 |___/`
 
 	app.Usage = ""
-	app.Version = "0.3.6"
+	app.Version = "0.4.0"
 	app.Commands = []cli.Command{
 		commands.TweetCommand(api, v),
 		commands.TimelineCommand(api, v),
